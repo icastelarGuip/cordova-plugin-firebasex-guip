f66d975cfdbab5e4cd44db261d68ee6912ae6a19
icastelarGuip <acastelar@guip.io>
2025-03-12 13:57:52 -0600

    FIX BoringSSL-GRPC
    Fix for older cordova installation

--- scripts/ios/helper.js
+++ scripts/ios/helper.js
@@ -295,6 +295,15 @@ post_install do |installer|
             if target.respond_to?(:product_type) and target.product_type == "com.apple.product-type.bundle"
                 config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
             end
+            if target.name == 'BoringSSL-GRPC'
+				target.source_build_phase.files.each do |file|
+				  if file.settings && file.settings['COMPILER_FLAGS']
+					flags = file.settings['COMPILER_FLAGS'].split
+					flags.reject! { |flag| flag == '-GCC_WARN_INHIBIT_ALL_WARNINGS' }
+					file.settings['COMPILER_FLAGS'] = flags.join(' ')
+				  end
+				end
+			end
         end
     end
 end
